96f9479dc645836ac8691934cee4af9e4d65a420
glossi <lhsddp@naver.com>
2023-01-18 21:35:54 +0900

    how to use styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { NextPage } from "next";
+import styled from "styled-components";
 
-const index = () => {
-  return <div>hello Typescript</div>;
+const Container = styled.div`
+  padding: 20px;
+`;
+
+const index: NextPage = () => {
+  return (
+    <Container>
+      <h1>hello styled-components</h1>
+      <h2>hello styled-components</h2>
+      <p>hello styled-components</p>
+      <ul>
+        <li>hello styled-components</li>
+      </ul>
+      <a>hello styled-components</a>
+      <span>hello styled-components</span>
+    </Container>
+  );
 };
 
 export default index;
