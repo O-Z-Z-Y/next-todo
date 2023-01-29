3afd7b862a3c7ddb7d72f96de2328b76757d7691
glossi <lhsddp@naver.com>
2023-01-29 15:46:58 +0900

    create Add todo-item

---
+++ pages/todo/add.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NextPage } from "next";
+import AddTodo from "../../components/AppTodo";
+
+const todo: NextPage = () => {
+  return <AddTodo />;
+};
+
+export default todo;
