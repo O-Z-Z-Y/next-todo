c817500b70e0c444f3ea9e9c67e1ccf9169aa0f4
glossi <lhsddp@naver.com>
2023-01-31 05:05:28 +0900

    use redux

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import TodoList from "../components/TodoList";
 import { TodoType } from "../types/todo";
 import { GetServerSideProps } from "next";
 import { getTodosAPI } from "../lib/api/todo";
+import { wrapper } from "../store";
+import { todoActions } from "../store/todo";
 
 interface IProps {
   todos: TodoType[];
@@ -12,14 +14,18 @@ const app: NextPage<IProps> = ({ todos }) => {
   return <TodoList todos={todos} />;
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  try {
-    const { data } = await getTodosAPI();
-    return { props: { todos: data } };
-  } catch (e) {
-    console.log(e);
-    return { props: {} };
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async () => {
+    console.log(store);
+    try {
+      const { data } = await getTodosAPI();
+      store.dispatch(todoActions.setTodo(data));
+      return { props: { todos: data } };
+    } catch (e) {
+      console.log(e);
+      return { props: {} };
+    }
   }
-};
+);
 
 export default app;
